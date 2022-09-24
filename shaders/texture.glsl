1f149541caba16b9a14245a350b7021fcbaa3145
Tachytaenius <henryfthomson@gmail.com>
2022-09-24 21:35:17 +0100

    Remove wrong line from texture shader

--- shaders/texture.glsl
+++ shaders/texture.glsl
@@ -18,7 +18,6 @@ vec4 effect(vec4 colour, sampler2D texture, vec2 textureCoords, vec2 windowCoord
 		if (1 - noise > fullness) { // The operation applied to noise is an aesthetic choice. Would not need to be done if fullness used a separate noise field
 			discard;
 		}
-		noise = (noise + 1) / 2.0;
 		noise = (noise - 0.5) * 2.0 * contrast + 0.5;
 		noise += brightness;
 		return vec4(colour.rgb * noise, 1.0);
