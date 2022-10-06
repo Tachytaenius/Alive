ae25c1ea8ea8ce563bf214787a7b18e904612311
Tachytaenius <henryfthomson@gmail.com>
2022-10-06 11:43:09 +0100

    Rename some variables

--- shaders/noise.glsl
+++ shaders/noise.glsl
@@ -3,14 +3,13 @@ uniform sampler2D noiseTexture;
 uniform float fullnessNoiseSize;
 uniform float fullnessNoiseOffset;
 
-// TODO: Prefix all of these with fragment
 varying vec2 fragmentPosition;
 varying vec4 fragmentColour;
-varying vec4 lightFilterColour;
-varying float noiseSize;
-varying float contrast;
-varying float brightness;
-varying float fullness; // Controls amount of pixels to discard
+varying vec4 fragmentLightFilterColour;
+varying float fragmentNoiseSize;
+varying float fragmentContrast;
+varying float fragmentBrightness;
+varying float fragmentNoiseFullness; // Controls amount of pixels to discard
 
 #ifdef VERTEX
 	attribute vec4 VertexLightFilterColour;
@@ -24,11 +23,11 @@ varying float fullness; // Controls amount of pixels to discard
 		
 		fragmentPosition = vertexPosition.xy;
 		fragmentColour = gammaCorrectColor(VertexColor);
-		lightFilterColour = gammaCorrectColor(VertexLightFilterColour);
-		noiseSize = VertexNoiseSize;
-		contrast = VertexContrast;
-		brightness = VertexBrightness;
-		fullness = VertexFullness;
+		fragmentLightFilterColour = gammaCorrectColor(VertexLightFilterColour);
+		fragmentNoiseSize = VertexNoiseSize;
+		fragmentContrast = VertexContrast;
+		fragmentBrightness = VertexBrightness;
+		fragmentNoiseFullness = VertexFullness;
 		
 		return transformedPosition;
 	}
@@ -39,18 +38,18 @@ varying float fullness; // Controls amount of pixels to discard
 	void effect() {
 		vec2 fullnessNoisePos = fragmentPosition / fullnessNoiseSize + fullnessNoiseOffset;
 		float fullnessNoise = Texel(noiseTexture, fullnessNoisePos / noiseTextureSize).r;
-		if (fullnessNoise > fullness) {
+		if (fullnessNoise > fragmentNoiseFullness) {
 			discard;
 		}
 		
-		vec2 noisePos = fragmentPosition / noiseSize;
+		vec2 noisePos = fragmentPosition / fragmentNoiseSize;
 		float noise = Texel(noiseTexture, noisePos / noiseTextureSize).r;
 		noise = noise * 2.0 - 1.0; // To [-1, 1]
-		noise *= contrast;
-		noise += brightness * 2.0 - 1.0;
+		noise *= fragmentContrast;
+		noise += fragmentBrightness * 2.0 - 1.0;
 		noise = noise / 2.0 + 0.5; // Back to [0, 1]
 		
 		love_Canvases[0] = vec4(fragmentColour.rgb * noise, fragmentColour.a); // Albedo
-		love_Canvases[1] = lightFilterColour; // Light filter. Rely on alpha to not change light filter canvas if not supposed to
+		love_Canvases[1] = fragmentLightFilterColour; // Light filter. Rely on alpha to not change light filter canvas if not supposed to
 	}
 #endif
