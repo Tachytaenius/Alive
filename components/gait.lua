20dda02dbf07d8d3e5ed8ef01fe35783ac085494
Tachytaenius <henryfthomson@gmail.com>
2022-10-11 14:38:54 +0100

    Change the way the gait component is constructed

--- components/gait.lua
+++ components/gait.lua
@@ -1,7 +1,8 @@
 local concord = require("lib.concord")
-return concord.component("gait", function(c, maxSpeed, acceleration, standThreshold, staggerRange)
+return concord.component("gait", function(c, maxSpeed, acceleration, standRange, staggerRange)
 	c.maxSpeed = maxSpeed
 	c.acceleration = acceleration
+	local standThreshold = maxSpeed + standRange
 	c.standThreshold = standThreshold -- Below this speed threshold the entity will get back on its feet
 	-- Between stand threshold and trip threshold would be the stagger range
 	c.tripThreshold = standThreshold + staggerRange -- Above this threshold the entity will fall over
