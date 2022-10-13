aaa119a11199b7578485cf8bed15aa1437321d10
Tachytaenius <henryfthomson@gmail.com>
2022-10-13 10:27:31 +0100

    Update love-game-boilerplate-lib

--- lib/love-game-boilerplate-lib/init.lua
+++ lib/love-game-boilerplate-lib/init.lua
@@ -427,6 +427,9 @@ function boilerplate.init(initConfig, arg)
 			end
 		end
 		-- It is in this code path here that we actually quit
+		if boilerplate.onQuit then
+			boilerplate.onQuit()
+		end
 		if boilerplate.killThreads then
 			log.info("Quitting threads...")
 			if boilerplate.killThreads() then
